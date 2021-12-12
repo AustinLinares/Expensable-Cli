2b5677c9bc7d3ba8a97c0ba66e034491ce3eaea6
AustinLinares <linaresaustin@hotmail.com>
2021-12-12 13:59:50 -0500

    date bugs fixed

--- category_handler.rb
+++ category_handler.rb
@@ -21,11 +21,11 @@ module CategoryHandlbegin
   def category_table
     # month_cat = @categories.select { |category| category.trans_in_month?(@current_month.to_s) }
     table = Terminal::Table.new
-    table.title = "#{@tr_type.capitalize}\n#{@current_month}"
+    table.title = "#{@tr_type.capitalize}\n#{@current_month.strftime("%B %Y")}"
     table.headings = ["ID", "Category", "Total"]
     tr_selected = @categories.select { |cat| cat.transaction_type == @tr_type}
     table.rows = tr_selected.map do |cat|
-      cat.month_row(@current_month.strftime("%-m"))
+      cat.month_row(@current_month.strftime("%Y-%m"))
     end
     puts table
   end
